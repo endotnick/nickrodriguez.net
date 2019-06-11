cee06760814a930277a5cff561638d44a8b59573
Nick Rodriguez <endotnick@gmail.com>
2019-06-10 21:25:01 -0700

    Update Vita.vue

--- src/components/Vita.vue
+++ src/components/Vita.vue
@@ -20,7 +20,7 @@ export default {
       md: '',
       pdf: 'https://drive.google.com/file/d/1dEBryepECkqFH41VRVib2XxHMEuPd9Ew/view',
       vita: 'https://gist.githubusercontent.com/endotnick/233613454f7a963f309dc50b4a74f496',
-      hash: 'd0455554969f80767d857c78306b009366665feb',
+      hash: '2fd85a45850a80e46b4668c9eac134d9bd2ee9cf',
       filename: 'nick_rodriguez_resume.md',
     };
   },
